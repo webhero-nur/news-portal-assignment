370d3287514d8f241f216e67792b9a258c5112c7
webhero-nur <nurealom522@gmail.com>
2022-10-30 20:50:50 +0600

    Cards added

--- js/nur.js
+++ js/nur.js
@@ -6,27 +6,64 @@ const categoriesData = () => {
 
 const categoryDetailView = categories => {
     const newsCategories = document.getElementById('news-categories');
+
     const categoriesDiv = document.createElement('div');
+
     const categoryUl = document.createElement('ul');
-    categoryUl.setAttribute("class", "list-style-none d-flex justify-content-between fw-semibold bg-light pe-4 py-2");
+    categoryUl.classList.add('list-style-none', 'd-flex', 'justify-content-between', 'fw-semibold', 'bg-light', 'px-4', 'py-2', 'mb-0');
     categoriesDiv.appendChild(categoryUl);
+
     const homeLi = document.createElement('li');
-    homeLi.classList.add('pointer');
-    homeLi.classList.add('px-1');
-    homeLi.innerText = 'Home';
-    homeLi.setAttribute("id", "home");
+    homeLi.classList.add('pointer', 'px-1');
+    homeLi.innerHTML = `
+        <div onclick=findNewsByCategory('home')>Home</div>
+    `;
     categoryUl.appendChild(homeLi);
+
     categories.forEach(category => {
-        const categoryLi = document.createElement('li');
-        categoryLi.classList.add('pointer');
-        categoryLi.classList.add('px-1');
-        const customId = `${category.category_id}`;
-        categoryLi.setAttribute("id", customId);
-        categoryLi.innerText = `${category.category_name}`;
-        categoryUl.appendChild(categoryLi);
+        const li = document.createElement('li');
+        li.classList.add('pointer', 'px-1');
+        li.innerHTML = `
+            <div onclick=findNewsByCategory('${category?.category_id}')>${category?.category_name}</div>
+        `;
+        categoryUl.appendChild(li);
     });
     newsCategories.appendChild(categoriesDiv);
-    console.log(categories);
+}
+
+const findNewsByCategory = newsId => {
+    const cards = document.getElementById('cards');
+    cards.textContent = '';
+    const findAllNewsByIdURL = `https://openapi.programming-hero.com/api/news/category/${newsId}`;
+    fetch(findAllNewsByIdURL)
+        .then(res => res.json())
+        .then(data => showNews(data.data))
+}
+
+const showNews = fullNewsAll => {
+    console.log(fullNewsAll);
+    // <div class="col">
+    //    <div class="card">
+    //       <img src="..." class="card-img-top" alt="...">
+    //       <div class="card-body">
+    //          <h5 class="card-title">Card title</h5>
+    //          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    //       </div>
+    //    </div>
+    // </div>
+    const cards = document.getElementById('cards');
+    fullNewsAll.forEach(fullNews => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        cardDiv.innerHTML = `
+            <img src="${fullNews.image_url}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        `;
+        cards.appendChild(cardDiv);
+    });
 }
 
 categoriesData();
